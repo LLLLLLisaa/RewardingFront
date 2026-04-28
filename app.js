427698ead54eafd0ce52514244a5c0da3318da48
LLLLLLisaa <xianochchu@gmail.com>
2026-04-28 15:24:44 +0200

    Add serviceWork in app.js

--- app.js
+++ app.js
@@ -242,6 +242,12 @@ detailCache = null;
 
 loadSummary();
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js")
+    .then(() => console.log("SW registered"))
+    .catch(err => console.log("SW error", err));
+}
+
 window.loadSummary = loadSummary;
 window.toggleDetail = toggleDetail;
 window.submitDuration = submitDuration;
